05c039f1fd552974b8ae7f022f020d22e8369d8b
RAM ANUJ PATHAK <109123656+PathakAnuj@users.noreply.github.com>
2023-07-10 08:58:40 +0530

    Updated Solution - GfG to GitHub

--- Easy/Transpose of Matrix/transpose-of-matrix.java
+++ Easy/Transpose of Matrix/transpose-of-matrix.java
@@ -35,20 +35,19 @@ class GFG
 // } Driver Code Ends
 
 
+
 //User function Template for Java
 class Solution
 {
     public void transpose(int n,int a[][])
     {
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int ram=a[i][j];
-                a[i][j]=a[j][i];
-                a[j][i]=ram;
-            }
+        for(int i=0;i<n-1;i++){
+          for(int j=i+1;j<n;j++){
+            int ab=a[i][j];
+            a[i][j]=a[j][i];
+            a[j][i]=ab;
+          }
         }
     }
-}
-
-
-
+}    
+    
