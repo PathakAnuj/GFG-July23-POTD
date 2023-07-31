c4d798f59ec9ddd49e6a7541b5b45a09b467145d
RAM ANUJ PATHAK <109123656+PathakAnuj@users.noreply.github.com>
2023-07-31 08:22:49 +0530

    Updated Solution - GfG to GitHub

--- Easy/BFS of graph/bfs-of-graph.java
+++ Easy/BFS of graph/bfs-of-graph.java
@@ -34,7 +34,6 @@ class GFG {
 
 
 
-
 class Solution {
     // Function to return Breadth First Traversal of given graph.
     public ArrayList<Integer> bfsOfGraph(int v, ArrayList<ArrayList<Integer>> adj) {
