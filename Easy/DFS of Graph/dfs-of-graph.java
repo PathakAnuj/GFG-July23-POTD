d2ca08e822ee473024fd6c5a98af7a52046e55a2
RAM ANUJ PATHAK <109123656+PathakAnuj@users.noreply.github.com>
2023-08-01 08:50:45 +0530

    Updated Solution - GfG to GitHub

--- Easy/DFS of Graph/dfs-of-graph.java
+++ Easy/DFS of Graph/dfs-of-graph.java
@@ -35,7 +35,6 @@ class GFG {
 
 
 
-
 class Solution {
     // Function to return a list containing the DFS traversal of the graph.
     public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
