96b4d80e03b547f1b84b57351037f8eb38ff4ea2
RAM ANUJ PATHAK <109123656+PathakAnuj@users.noreply.github.com>
2023-08-16 08:12:13 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Nth catalan number/nth-catalan-number.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            Solution obj = new Solution();
+            int res = obj.findCatalan(n);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+
+class Solution {
+    public int findCatalan(int n) {
+        if (n == 0) {
+            return 1;
+        }      
+        final int MOD = (int) (1e9 + 7);
+        long[] v = new long[n + 1];
+        v[0] = v[1] = 1;
+        
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                v[i] = (v[i] + (v[j] * v[i - j - 1]) % MOD) % MOD;
+            }
+        }  
+        return (int) v[n];
+    }
+}
+        
+        
