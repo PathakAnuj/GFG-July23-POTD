930c3b41227b454c07c3d06119c50248e7c83214
RAM ANUJ PATHAK <109123656+PathakAnuj@users.noreply.github.com>
2023-09-03 09:40:08 +0530

    Updated Solution - GfG to GitHub

--- Easy/Check if Tree is Isomorphic/check-if-tree-is-isomorphic.java
+++ Easy/Check if Tree is Isomorphic/check-if-tree-is-isomorphic.java
@@ -114,27 +114,33 @@ class GfG {
 // } Driver Code Ends
 
 
-class Solution {
-    private boolean areIsomorphicUtil(Node root1, Node root2) {
-        if (root1 == null && root2 == null)
-            return true;
-        if (root1 == null || root2 == null)
-            return false;
 
-        if (root1.data != root2.data)
-            return false;
 
-        boolean case1 = areIsomorphicUtil(root1.left, root2.left) &&
-                        areIsomorphicUtil(root1.right, root2.right);
+//User function Template for Java
 
-        boolean case2 = areIsomorphicUtil(root1.left, root2.right) &&
-                        areIsomorphicUtil(root1.right, root2.left);
-
-        return case1 || case2;
+class Solution  
+{ 
+    // Return True if the given trees are isomotphic. Else return False.
+    boolean isIsomorphic(Node root1, Node root2)  
+    { 
+         // code here.
+         if(root1==null && root2==null){
+             return true;
+         }
+         if(root1==null || root2==null){
+             return false;
+         }
+         if(root1.data!=root2.data){
+             return false;
+         }
+         if(isIsomorphic(root1.left,root2.left) && isIsomorphic(root1.right,root2.right)){
+             return true;
+         }
+         if(isIsomorphic(root1.left,root2.right) && isIsomorphic(root1.right,root2.left)){
+             return true;
+         }
+         return false;
+         
     }
-    boolean isIsomorphic(Node root1, Node root2) {
-        if (areIsomorphicUtil(root1, root2))
-            return true;
-        return false;
-    }
-} 
+    
+}       
