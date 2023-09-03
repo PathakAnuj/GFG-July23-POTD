f3b29f876e09e7cc3599e04aedb5fd566db9d901
RAM ANUJ PATHAK <109123656+PathakAnuj@users.noreply.github.com>
2023-09-03 09:42:57 +0530

    Updated Solution - GfG to GitHub

--- Easy/Check if Tree is Isomorphic/check-if-tree-is-isomorphic.java
+++ Easy/Check if Tree is Isomorphic/check-if-tree-is-isomorphic.java
@@ -116,31 +116,22 @@ class GfG {
 
 
 
-//User function Template for Java
-
 class Solution  
 { 
     // Return True if the given trees are isomotphic. Else return False.
     boolean isIsomorphic(Node root1, Node root2)  
     { 
-         // code here.
-         if(root1==null && root2==null){
-             return true;
-         }
-         if(root1==null || root2==null){
-             return false;
-         }
-         if(root1.data!=root2.data){
-             return false;
-         }
-         if(isIsomorphic(root1.left,root2.left) && isIsomorphic(root1.right,root2.right)){
-             return true;
-         }
-         if(isIsomorphic(root1.left,root2.right) && isIsomorphic(root1.right,root2.left)){
-             return true;
-         }
-         return false;
-         
+        if(root1 == null && root2 == null)
+            return true;
+        
+        if(root1 == null || root2 == null)
+            return false;
+        
+        if(root1.data != root2.data)
+            return false;
+        
+        
+        return (isIsomorphic(root1.left, root2.left) && isIsomorphic(root1.right, root2.right)) || (isIsomorphic(root1.left, root2.right) && isIsomorphic(root1.right, root2.left));
     }
     
-}       
+}    
