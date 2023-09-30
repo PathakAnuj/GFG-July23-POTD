1eeec4954f5334da837f62b25f2f9962d2c71e8a
RAM ANUJ PATHAK <109123656+PathakAnuj@users.noreply.github.com>
2023-09-30 12:16:03 +0530

    Updated Solution - GfG to GitHub

--- Medium/Boolean Matrix/boolean-matrix.java
+++ Medium/Boolean Matrix/boolean-matrix.java
@@ -43,12 +43,9 @@ class GFG
 // } Driver Code Ends
 
 
-
-
 class Solution
 {
-    //Function to modify the matrix such that if a matrix cell matrix[i][j]
-    //is 1 then all the cells in its ith row and jth column will become 1.
+
     void booleanMatrix(int matrix[][])
     {
         // code here 
