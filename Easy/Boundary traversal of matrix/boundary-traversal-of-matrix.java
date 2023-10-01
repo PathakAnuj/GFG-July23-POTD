dfeb25b9f01b4511860d05022c50101fb6f69609
RAM ANUJ PATHAK <109123656+PathakAnuj@users.noreply.github.com>
2023-10-01 15:45:18 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Boundary traversal of matrix/boundary-traversal-of-matrix.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine().trim());
+        
+        while(t-- > 0)
+        {
+            String s[] = in.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int m = Integer.parseInt(s[1]);
+            int a[][] = new int[n][m];
+            s = in.readLine().trim().split(" ");
+            int ind=0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    a[i][j] = Integer.parseInt(s[ind++]);
+                }
+            }
+            Solution ob = new Solution();
+            ArrayList<Integer> ans = ob.boundaryTraversal(a, n, m);
+            for (int i : ans) {
+                out.print(i + " ");
+            }
+            out.println();
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to return list of integers that form the boundary 
+    //traversal of the matrix in a clockwise manner.
+    static ArrayList<Integer> boundaryTraversal(int matrix[][], int n, int m)
+    {
+        ArrayList<Integer> arr = new ArrayList<>();
+        int i=0,j=0;
+        while(j<m) arr.add(matrix[i][j++]);
+        j--;
+        i++;
+        while(i<n) arr.add(matrix[i++][j]);
+        i--;
+        j--;
+        while(j>=0) arr.add(matrix[i][j--]);
+        j++;
+        i--;
+        while(i>=1) arr.add(matrix[i--][j]);
+        while(arr.size()>n*m){
+            arr.remove(arr.size()-1);
+        }
+        return arr;
+    }
+}
