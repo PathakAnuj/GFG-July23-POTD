bb2a090bd9caa9e8c86f885b8959be4d7465ad12
RAM ANUJ PATHAK <109123656+PathakAnuj@users.noreply.github.com>
2023-10-28 00:02:35 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Bleak Numbers/bleak-numbers.java
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            int ans = ob.is_bleak(n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class Solution
+{
+   public int is_bleak(int n)
+   {
+       // Code here
+       for (int x = n - (int)Math.ceil(Math.log(n)/Math.log(2)); x < n; x++)
+           if (x + Integer.bitCount(x) == n)
+               return 0;
+       return 1;
+   }
+}
